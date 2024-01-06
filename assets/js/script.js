049b490bb07d16867cfa6913e23c6a0585557822
hszilvi <szilvi@naszta.hu>
2024-01-06 21:31:51 +0000

    convert data into correct formats in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,60 @@
-// const dayjs = require('dayjs');
-
-// var advancedFormat = require('dayjs/plugin/advancedFormat')
-
-
-console.log('it works')
+// add current date to header, format: "Saturday, January 6th" 
+// extra script needed in html (advanced dayjs)
 const today = dayjs();
 console.log(today)
 $('#currentDay').text(today.format("dddd[,] MMMM Do"));
 
-// link the colores to current date if current hour is equal to 
+
+// ----------------------------------
+// link the colors to current date if current hour is equal to 
+// current hour red, past hours gray, future hours green
+// create variable to store the current hour / than compare the elements of the calendar to it
+const currentHour = today.format("hA");
+// const currentHour = '10AM';
+console.log('currenthour ' + currentHour);
+// need only the values of currentHour object > example: array ["8", 'P', 'M']
+const currentHourToArray = Object.values(currentHour);
+console.log('string current ' + currentHourToArray);
+console.log(typeof{currentHourToArray});
+const currentHourArraytoString = currentHourToArray.join("");
+console.log('end ' +currentHourArraytoString);
+
+// need a variable to get hour classes from html and have to split them by letters to make them comparable to current hour
+const calendarHour = $('.hour');
+
+
+// const splittedCalendarHour = calendarHour.split(' ');
+const calendarHourToString = Object.values(calendarHour);
+console.log('calendarhoutostring ' + calendarHourToString);
+// console.log('splitted' + splittedCalendarHour);
+const task =$('.task');
+
+// calendarHourToString.forEach(hour => {
+//     console.log(hour.innerText);
+// })
+
+const noon = $(".hour-noon");
+console.log('noon ' + noon.text());
+
+calendarHourToString.forEach(hour => {
+    if (hour.innerText === currentHourArraytoString) {
+    task.addClass('present');
+    console.log('its present')
+} else {
+    console.log('not the same hour')
+    // i have to compare the child elements  - nth hour is above or below the present element
+}
+
+
+// else if (currentHourArraytoString > hour.innerText) {
+//     task.addClass('past');
+//     console.log('its past')
+// } else {
+//     task.addClass('future');
+//     console.log('its future')
+// }
+});
+
 
 // saveBtn to save task into local storage
 $('.saveBtn').on('click', function() {
